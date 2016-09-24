ee52ea84a18e52dc8de10fd42dbb06bb9d40e113
denbeigh <denbeigh@denbeighstevens.com>
2016-09-24 15:35:48 -0700

    added cmd line arguments for port

--- cmd/storage-http/main.go
+++ cmd/storage-http/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/denbeigh2000/jfsi/storage/disk"
 	"github.com/denbeigh2000/jfsi/storage/handler"
 )
 
+var port = flag.Int("port", 8080, "Port to serve on")
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 	store := disk.NewDiskStore("./.jfsi")
 	handler := handler.NewHTTP(store)
 
-	http.ListenAndServe(":8080", handler)
+	http.ListenAndServe(fmt.Sprintf(":%v", *port), handler)
 }
