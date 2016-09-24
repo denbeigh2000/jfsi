a7c8855421423196aa417c1949c1460c2b8f7d95
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2016-09-24 16:12:26 -0700

    Added command line flag for storage location

--- cmd/storage-http/main.go
+++ cmd/storage-http/main.go
@@ -9,14 +9,17 @@ import (
 	"github.com/denbeigh2000/jfsi/storage/handler"
 )
 
-var port = flag.Int("port", 8080, "Port to serve on")
+var (
+	port = flag.Int("port", 8080, "Port to serve on")
+	dir  = flag.String("port", "./.jfsi", "Directory to store blobs in")
+)
 
 func init() {
 	flag.Parse()
 }
 
 func main() {
-	store := disk.NewDiskStore("./.jfsi")
+	store := disk.NewDiskStore(*dir)
 	handler := handler.NewHTTP(store)
 
 	http.ListenAndServe(fmt.Sprintf(":%v", *port), handler)
